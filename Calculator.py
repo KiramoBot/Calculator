a3f5b2c5f3d7e9256bd43aee2036365dda811d23
Christoph Ramke <78666140+KiramoBot@users.noreply.github.com>
2021-02-07 20:10:31 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,8 +4,8 @@ root.title("Simple Calculator")
 
 # INPUT WINDOW ****************************************************************************************************************
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e = Entry(root, width=35, borderwidth=5, bg="black", fg="white")
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 # FUNCTIONS *********************************************************************************************************************
 
@@ -17,46 +17,70 @@ def button_click(number):
 def button_clear():
     e.delete(0, END)
 
-def button_add():
-    first_number = e.get()
-    global f_num
-    f_num = int(first_number)
-    e.delete(0, END)
-
 def button_equal():
     second_number = e.get()
     e.delete(0, END)
-    e.insert(0, f_num + int(second_number))
+    if math == "addition":
+        e.insert(0, f_num + int(second_number))
+    elif math == "subtraction":
+        e.insert(0, f_num - int(second_number))
+    elif math == "multiplication":
+        e.insert(0, f_num * int(second_number))
+    elif math == "division":
+        e.insert(0, f_num / int(second_number))
+
+def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 def button_subtract():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 def button_multiply():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 def button_divide():
-    return
+    first_number = e.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 # DEFINE BUTTONS *****************************************************************************************************************
 
-button1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
-button2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
-button3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
-button4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
-button5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
-button6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
-button7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
-button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
-button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1), bg="gray",fg="red")
+button2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2), bg="gray",fg="red")
+button3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3), bg="gray",fg="red")
+button4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4), bg="gray",fg="red")
+button5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5), bg="gray",fg="red")
+button6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6), bg="gray",fg="red")
+button7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7), bg="gray",fg="red")
+button8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8), bg="gray",fg="red")
+button9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9), bg="gray",fg="red")
+button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0), bg="gray",fg="red")
 
-button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add, bg="green",fg="black")
+button_equal = Button(root, text="=", padx=75, pady=20, command=button_equal, bg="white")
+button_clear = Button(root, text="Clear", padx=66, pady=20, command=button_clear,bg="white",fg="black")
 
-button_subtract = Button(root, text="-", padx=39, pady=20, command=button_subtract)
-button_multiply = Button(root, text="*", padx=39, pady=20, command=button_multiply)
-button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide)
+button_subtract = Button(root, text="-", padx=41, pady=20, command=button_subtract, bg="green",fg="black")
+button_multiply = Button(root, text="*", padx=39, pady=20, command=button_multiply, bg="green",fg="black")
+button_divide = Button(root, text="/", padx=39, pady=20, command=button_divide, bg="green",fg="black")
 
 # IMPLEMENT THE BUTTONS ***********************************************************************************************************
 
@@ -77,9 +101,9 @@ button_clear.grid(row=4, column=1, columnspan=2)
 button_equal.grid(row=5, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 
-
-
-
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 
 
