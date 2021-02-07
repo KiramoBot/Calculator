2f9fc11beeeb9bf596829757d43fcb017ec28226
Christoph Ramke <78666140+KiramoBot@users.noreply.github.com>
2021-02-07 19:34:41 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,7 @@ root.title("Simple Calculator")
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-# FUNCTION *********************************************************************************************************************
+# FUNCTIONS *********************************************************************************************************************
 
 def button_click(number):
     current = e.get()
